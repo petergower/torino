0637ba5967596baee218fd0cdf328a1814fb0ed2
Edoardo Paolo Scalafiotti <edoardo849@gmail.com>
2016-11-29 11:38:12 +0000

    Added docker instructions

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# torino
+Torino
+===
+
+Start the docker daemon
+
+```bash
+
+# <BS>https://wiki.archlinux.org/index.php/Docker#Installation
+sudo gpasswd -a USER docker
+
+# logout, login
+newgrp docker
+
+systemctl start docker
+
+# to close
+
+systemctl stop docker
+
+sudo dockerd
+
+```
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cmd := exec.Command("tr", "a-z", "A-Z")
+	cmd.Stdin = strings.NewReader("some input")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+	startEngine()
+}
+
+func startEngine() {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
+}
